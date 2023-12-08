0f8d5ab75426535861a3637cf551aa6dae9e1ade
Dinethra Sulakshana <dinethrasulakshana@gmail.com>
2023-12-08 09:23:54 +0530

    updated DeleteEventServlet and UpdateBookingServlet

--- EventManagmentDemo/src/main/java/com/oop/servlet/DeleteEventServlet.java
+++ EventManagmentDemo/src/main/java/com/oop/servlet/DeleteEventServlet.java
@@ -1,41 +1,37 @@
 package com.oop.servlet;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class DeleteEventServlet
- */
+import com.oop.util.EventDBUtil;
+
+
+
 @WebServlet("/DeleteEventServlet")
 public class DeleteEventServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public DeleteEventServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		String id = request.getParameter("eventid");
+		boolean isTrue;
+		
+		isTrue = EventDBUtil.deleteEvent(id);
+		
+		if(isTrue == true) {
+			RequestDispatcher dis = request.getRequestDispatcher("deletesuccess.jsp");
+			dis.forward(request,response);	
+		}
+		else {
+			RequestDispatcher dis2 = request.getRequestDispatcher("deleteunsuccess.jsp");
+			dis2.forward(request,response);	
+		}
 	}
 
 }
--- EventManagmentDemo/src/main/java/com/oop/servlet/UpdateBookingServlet.java
+++ EventManagmentDemo/src/main/java/com/oop/servlet/UpdateBookingServlet.java
@@ -1,41 +1,50 @@
 package com.oop.servlet;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class UpdateBookingServlet
- */
+import com.oop.model.Booking;
+import com.oop.util.bookingDBUtill;
+
 @WebServlet("/UpdateBookingServlet")
 public class UpdateBookingServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public UpdateBookingServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+    
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		String bookingid = request.getParameter("bookingid");
+		String cusid = request.getParameter("cusid");
+		String venue = request.getParameter("venue");
+		String date = request.getParameter("date");
+		String event = request.getParameter("event");
+		int capacity = Integer.parseInt(request.getParameter("capacity"));
+		//String capacity = request.getParameter("capacity");
+		
+		boolean isTrue;
+		
+		isTrue = bookingDBUtill.updatebooking(bookingid, cusid, venue, date, event,capacity);
+		
+		
+		if(isTrue == true) {
+			
+			List<Booking> bookingDetails = bookingDBUtill.getBookingDetails(bookingid);
+			request.setAttribute("BookingDetails", bookingDetails);
+			
+			RequestDispatcher dis = request.getRequestDispatcher("bookingdata.jsp");
+			dis.forward(request,response);
+		}else {
+			List<Booking> bookDetails = bookingDBUtill.getBookingDetails(bookingid);
+			request.setAttribute("BookingDetails", bookDetails);
+			
+			RequestDispatcher dis2 = request.getRequestDispatcher("bookingdata.jsp");
+			dis2.forward(request,response);
+		}
 	}
 
 }
